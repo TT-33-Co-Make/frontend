23e93a713c8b3cd1bd21a6b6ee06be627c4a951c
Oscar Figueroa <Osc.Fig96@gmail.com>
2021-02-01 11:07:50 -0800

    Added a few routes/links to App.js and NavBar.js.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Switch, Redirect} from 'react-router-dom';
 import './App.css';
+
+// Componenets
+import Home from './components/Home'
+import NavBar from './components/NavBar'
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello</p>
+      <NavBar />
+
+      <Switch>
+        <Route path='/login' />
+        <Route path='/signup' />
+        
+        <Route path='/' component={Home} />
+      </Switch>
     </div>
   );
 }
