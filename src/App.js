8a67a805ef3901779db0a4dfcf5c1779beb3c2ca
Plofland <ploflan@gmail.com>
2021-02-01 20:11:18 -0500

    Changed the structure of the Issue components so that we can display each issue, made an IssueList component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-
-import React, {useState} from 'react';
-import { Route, Switch, Redirect} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import './App.css';
 import Signup from './components/Signup';
 
 // Contexts
-import {AuthContext} from './contexts/AuthContext';
+import { AuthContext } from './contexts/AuthContext';
 
 // Componenets
-import Home from './components/Home'
-import NavBar from './components/NavBar'
-import Login from './components/Login'
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import Login from './components/Login';
+// import IssuePage from './components/IssuePage';
 
 function App() {
-
-
   return (
     <div className="App">
       <AuthContext.Provider value={{}}>
@@ -23,10 +21,11 @@ function App() {
         <NavBar />
 
         <Switch>
-          <Route path='/developers'/>
-          <Route path='/login' component={Login}/>
-          <Route path='/signup' component={Signup} />
-          <Route path='/' component={Home} />
+          <a href="/issues">Issue List</a>
+          <Route path="/developers" />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/" component={Home} />
         </Switch>
       </AuthContext.Provider>
     </div>
---
+++ src/components/IssueCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+function Issue({ issue }) {
+  return (
+    <IssueCard>
+      <h3>DummyTitle{issue.title}</h3>
+      {/* <h3>{issue.title}</h3> */}
+      <p>
+        Solve challenges Action Against Hunger citizenry Martin Luther King Jr.
+        Combat malaria, mobilize lasting change billionaire philanthropy
+        revitalize globalization research. Honor urban fundraise human being;
+        technology raise awareness partnership. Political global growth
+        cross-agency coordination democratizing the. Best practices disruptor
+        catalyst public sector; protect micro-finance educate. Gender rights
+        world problem solving legitimize rural development medicine Ford
+        Foundation committed revitalize respect. Affordable health care,
+        humanitarian.
+      </p>
+      {/* <p>{issue.description}</p> */}
+    </IssueCard>
+  );
+}
+
+export default Issue;
+
+const IssueCard = styled.div`
+  display: flex;
+  justify-content: center;
+`;
