ce2e736b1fc47567dfe31edc6310ad16f8caf357
Plofland <ploflan@gmail.com>
2021-02-02 19:29:13 -0500

    Style changes to issue card

--- src/components/IssueList.js
+++ src/components/IssueList.js
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 import IssueCard from './IssueCard';
+import axiosWithAuth from '../utils/axiosWithAuth';
 
 function IssuePage() {
   const [issuesList, setIssuesList] = useState([]);
 
   useEffect(() => {
-    axios
-      .get('https://co-make-tt-33.herokuapp.com/api/issues')
+    axiosWithAuth()
+      .get('issues')
       .then((res) => {
         console.log('GET ISSUES', res);
         // setIssuesList(res.data);
@@ -33,10 +34,20 @@ const IssueList = styled.div`
   border: 2px solid black;
   width: 80%;
   margin: 2% auto;
-  padding: 2%;
+  padding: 1% 2%;
+  background-color: #28a745;
   box-shadow: 
-      /* top */ rgba(0, 0, 0, 0.1) 0px -6px 10px,
-    /* right */ rgba(0, 0, 0, 0.1) 6px 0px 10px,
-    /* bottom */ rgba(0, 0, 0, 0.1) 0px 6px 10px,
-    /* left */ rgba(0, 0, 0, 0.1) -6px 0px 10px;
+    /* top */ rgba(0, 0, 0, 0.2) 0px -6px 10px,
+    /* right */ rgba(0, 0, 0, 0.2) 6px 0px 10px,
+    /* bottom */ rgba(0, 0, 0, 0.2) 0px 6px 10px,
+    /* left */ rgba(0, 0, 0, 0.2) -6px 0px 10px;
+
+  :hover {
+    transition: 0.4s ease-in-out;
+    box-shadow: 
+      /* top */ rgba(0, 0, 0, 0.5) 0px -6px 20px,
+      /* right */ rgba(0, 0, 0, 0.5) 6px 0px 20px,
+      /* bottom */ rgba(0, 0, 0, 0.5) 0px 6px 20px,
+      /* left */ rgba(0, 0, 0, 0.5) -6px 0px 20px;
+  }
 `;
