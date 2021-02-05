a682a195dcc811c62046674e9acba0d8c69d6c4e
Plofland <ploflan@gmail.com>
2021-02-05 16:15:33 -0500

    Changed Login and Signup axios call back to what they should be

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const axiosWithAuth = () => {
   const token = sessionStorage.getItem('token');
+
   return axios.create({
     baseURL: 'https://comake-backend-lambda.herokuapp.com/api/',
     headers: {
